f8d5bcd3f1f6cea14a015f977fc5550e03e3e162
OvercomingJunk <overcomingjunk@yandex.ru>
2023-01-12 17:52:43 +0300

    HTML generator

--- src/main/java/ru/job4j/dreamjob/controller/IndexController.java
+++ src/main/java/ru/job4j/dreamjob/controller/IndexController.java
@@ -1,17 +1,17 @@
 package ru.job4j.dreamjob.controller;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
 
 /**
  * @author Artem Chernikov
  * @version 1.0
  * @since 11.01.2023
  */
-@RestController
+@Controller
 public class IndexController {
     @GetMapping("/index")
     public String getIndex() {
-        return "Hello World!";
+        return "index";
     }
 }
